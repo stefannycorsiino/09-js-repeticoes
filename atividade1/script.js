6fdea64c6c050c308e51cc8df6bba0a8ba146ed7
stefannycorsiino <132668930+stefannycorsiino@users.noreply.github.com>
2023-07-09 19:28:11 +0000

    Stefanny

--- atividade1/script.js
+++ atividade1/script.js
@@ -6,12 +6,17 @@ function contar(){
 
     if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
        window.alert('[ERRO] Faltam dados')
+       res.innerHTML = `Impossivel contar!`
        
     } else {
-        res.innerHTML = "Contando:"
+        res.innerHTML = "Contando: <br>"
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        if (p <= 0){
+            window.alert('Passo Invalido! Considerando PASSO 1')
+            p = 1
+        }
         if (i < f){
              for (let c = i; c <= f;  c += p){
             res.innerHTML += `${c} \u{1F449}`
